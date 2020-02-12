4af31b4e717b17d033d3ad8c851c10e014ac2b7c
Shadow <taltaro2@gmail.com>
2020-02-12 10:55:05 -0500

    added Js

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+let greenSection = document.querySelector("#topLeft");
+let redSection = document.querySelector("#topRight");
+let blueSection = document.querySelector("#bottomRight");
+let yellowSection = document.querySelector("#bottomLeft");
+
+let start = document.querySelector("#start")
+let reset = document.querySelector("#reset")
+
+var greenAudio = document.querySelector("#greenAudio")
+var redAudio = document.querySelector("#redAudio")
+var blueAudio = document.querySelector("#blueAudio")
+var yellowAudio = document.querySelector("#yellowAudio")
+
+const green = greenAudio, greenSection
+const red = redAudio, redSection
+const blue = blueAudio, blueSection
+const yellow = yellowAudio, yellowSection
+
