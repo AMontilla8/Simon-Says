5c25aca3c6d1c955d1f23e6fcd3a70eb07e8f749
Shadow <taltaro2@gmail.com>
2020-02-13 15:34:54 -0500

    flashes random panels Js

--- script.js
+++ script.js
@@ -4,23 +4,53 @@
 
 
 
-let greenSection = document.querySelector("#topLeft");
-let redSection = document.querySelector("#topRight");
-let blueSection = document.querySelector("#bottomRight");
-let yellowSection = document.querySelector("#bottomLeft");
+    let topLeft = document.querySelector("#topLeft");
+    let topRight = document.querySelector("#topRight");
+    let bottomRight = document.querySelector("#bottomRight");
+    let bottomLeft = document.querySelector("#bottomLeft");
 
-let start = document.querySelector("#start")
-let reset = document.querySelector("#reset")
+    let start = document.querySelector("#start")
+    let reset = document.querySelector("#reset")
 
-var greenAudio = document.querySelector("#greenAudio")
-var redAudio = document.querySelector("#redAudio")
-var blueAudio = document.querySelector("#blueAudio")
-var yellowAudio = document.querySelector("#yellowAudio")
+    var greenAudio = document.querySelector("#greenAudio")
+    var redAudio = document.querySelector("#redAudio")
+    var blueAudio = document.querySelector("#blueAudio")
+    var yellowAudio = document.querySelector("#yellowAudio")
 
-topleft.addEventListener('click', (event) => {
-    if (on) {
-        
-    }
-})
 
-let green = new topleft ("#topleft", "#greenAudio")
+
+    const sequences = [ 
+        getRandomPanel(),
+         getRandomPanel(),
+          getRandomPanel(),
+           getRandomPanel()
+    ];
+    
+    const panel = [
+        topLeft, topRight, bottomRight, bottomLeft
+    ];
+    
+    const getRandomPanel = () => {
+        const panels = [
+          topLeft,
+          topRight,
+          bottomRight,
+          bottomLeft
+        ];
+        return panels[parseInt(Math.random() * panels.length)];
+      };
+
+    const flash = panel => {
+        return new Promise((resolve, reject) =>{
+        panel.className += 'active';
+        setTimeout(() => {
+            panel.className = board.className.replace('active',
+            ''
+            );
+                setTimeout(() => {
+            resolve();
+                }, 250);
+        }, 500);
+    });
+};
+
